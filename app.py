272454eca59e98ca678a9c79b5ca328de4ee54c1
JeffersonSantos <104048237+daylerjeff199906@users.noreply.github.com>
2025-05-22 09:25:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from PIL import Image as PILImage
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}}
+CORS(app, resources={r"/*": {"origins": "https://vertebrados.iiap.gob.pe"}})
 
 API_BASE = 'https://api.vertebrados.iiap.gob.pe/api/v1/individuals/'
 
